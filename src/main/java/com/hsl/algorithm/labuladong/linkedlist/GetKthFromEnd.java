8850863b75e03aba4f87dd5621ad3d8c268c034a
haoshaolong <haoshaolong@pin-dao.cn>
2022-11-22 10:07:18 +0800

    链表倒数第k个节点

---
+++ src/main/java/com/hsl/algorithm/labuladong/linkedlist/GetKthFromEnd.java
@@ -0,0 +1,39 @@
+package com.hsl.algorithm.labuladong.linkedlist;
+
+import com.hsl.offer.base.ListNode;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ */
+public class GetKthFromEnd {
+
+    public static void main(String[] args) {
+        GetKthFromEnd main = new GetKthFromEnd();
+        int[] nodes = {1,2,3,4,5};
+        ListNode kthFromEnd = main.getKthFromEnd(ListNode.buildNode(nodes), 2);
+        System.out.println(kthFromEnd.toString());
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        if (head == null || k < 1) {
+            return null;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head;
+        // 先走k-1步
+        for (int i = 0; i < k-1 && p1 != null; i++) {
+            p1 = p1.next;
+        }
+        // 如果k大于链表长度，则返回null
+        if (p1 == null) {
+            return null;
+        }
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2= p2.next;
+        }
+        return p2;
+    }
+}

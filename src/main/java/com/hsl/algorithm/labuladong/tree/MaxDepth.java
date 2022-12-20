6ee0a7f23a5c534a1e202834792f2a782a8060e3
haoshaolong <haoshaolong@pin-dao.cn>
2022-12-20 09:18:04 +0800

    104. 二叉树的最大深度

--- src/main/java/com/hsl/algorithm/labuladong/tree/MaxDepth.java
+++ src/main/java/com/hsl/algorithm/labuladong/tree/MaxDepth.java
@@ -11,8 +11,36 @@ import com.hsl.algorithm.base.TreeNode;
  */
 public class MaxDepth {
 
-    public int maxDepth(TreeNode root) {
-
-        return 0;
+    public static void main(String[] args) throws Exception {
+        MaxDepth main = new MaxDepth();
+        String[] preOrders = {"3","9","20","15","7"};
+        String[] midOrders = {"9","3","15","20","7"};
+        TreeNode root = TreeNode.buildTreeWithPreMidOrder(preOrders, midOrders);
+        int maxDepth = main.traverse2(root);
+        System.out.println(maxDepth == 3);
     }
+
+    public int maxDepth1(TreeNode root) {
+        return this.traverse1(root, 0);
+    }
+
+    private int traverse1(TreeNode root, int depth) {
+        if (root == null) {
+            return depth;
+        }
+        depth++;
+        return Math.max(traverse1(root.left, depth), traverse1(root.right, depth));
+    }
+
+    public int maxDepth2(TreeNode root) {
+        return this.traverse2(root);
+    }
+
+    private int traverse2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(traverse2(root.left), traverse2(root.right)) + 1;
+    }
+
 }

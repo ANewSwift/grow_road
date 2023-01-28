a51f5d2202a3c79f9de0e1efcd83465da9cda7ea
haoshaolong <haoshaolong@pin-dao.cn>
2023-01-28 16:42:00 +0800

    111. 二叉树的最小深度 新增方法

--- src/main/java/com/hsl/algorithm/labuladong/bfs/MinDepth.java
+++ src/main/java/com/hsl/algorithm/labuladong/bfs/MinDepth.java
@@ -3,6 +3,7 @@ package com.hsl.algorithm.labuladong.bfs;
 import com.hsl.algorithm.base.TreeNode;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 111. 二叉树的最小深度
@@ -28,9 +29,32 @@ public class MinDepth {
 //        TreeNode root = TreeNode.buildTree(new String[]{"2", null, "3", null, "4", null, "5", null, "6"});
 //        System.out.println(main.minDepth(root) == 5);
         TreeNode root = TreeNode.buildTree(new String[]{"1", "2", "3", "4", null, null, "5"});
-        System.out.println(main.minDepth(root) == 3);
+        System.out.println(main.minDepth2(root) == 3);
     }
 
+    public int minDepth2(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
         int depth = 1;
